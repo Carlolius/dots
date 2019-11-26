9a9f481b37ed88b4f57d69a30ef300ace5909216
Carlos <carloshcr2@gmail.com>
2019-11-26 19:39:53 +0100

    added screenlock script

---
+++ scripts/screenlock.sh
@@ -0,0 +1,24 @@
+#!/usr/env/bin bash
+
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(xrandr | grep primary | cut -f 1 -d ' ')"
+
+# Run xidlehook
+xidlehook \
+	`# Don't lock when fullscreen` \
+	--not-when-fullscreen \
+	`# Dim the screen after X seconds, undim if user becomes active` \
+	--timer normal \
+	'180' \
+	'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+	'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+	`# Lock screen after another X seconds, full bright if user active` \
+	--timer normal \
+	'60' \
+	'i3lock -c 000000' \
+	'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+	`# Turn off screen after another X seconds, full bright if user active` \
+	--timer normal \
+	'10' \
+	'xset dpms force off' \
+	'xrandr --output "$PRIMARY_DISPLAY" --brightness 1'
