51711df4867fece4d189227c7bdfd083759ea772
Carlos <carloshcr2@gmail.com>
2020-01-08 02:11:48 +0100

    updated screenlock script

--- files/scripts/screenlock.sh
+++ files/scripts/screenlock.sh
@@ -8,20 +8,17 @@ xidlehook \
 	`# Don't lock when fullscreen` \
 	--not-when-fullscreen \
 	`# Dim the screen after X seconds, undim if user becomes active` \
-	--timer normal \
-	'180' \
-	`#xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
-	'feh -F ~/dots/media/aperture_laboratories.jpg' \
-	`#xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
-	'killall feh' \
+	--timer	180180180180180180180180180180180180180180180180180180 \
+	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
+	  'feh -F ~/dots/media/aperture_laboratories.jpg' \
+	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
+	  'killall feh' \
 	`# Lock screen after another X seconds, full bright if user active` \
-	--timer normal \
-	'60' \
-	'i3lock -c 000000' \
-	'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+	--timer 60 \
+	  'i3lock -c 000000' \
+	  'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
 	`# Turn off screen after another X seconds, full bright if user active` \
-	--timer normal \
-	'10' \
-	'xset dpms force off' \
-	'killall feh' \
-	`#xrandr --output "$PRIMARY_DISPLAY" --brightness 1`
+	--timer 10 \
+	  'xset dpms force off' \
+	  'killall feh' \
+	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1`
