51d3b8d2628938736dce954564a95a9a46762a93
Carlos <carloshcr2@gmail.com>
2020-02-04 17:02:39 +0100

    Edits on the screenlock script

---
+++ files/scripts/lock.sh
@@ -0,0 +1,1 @@
+xidlehook --timer 1 'feh -F ~/dots/media/aperture_laboratories.jpg' 'killall feh' --timer 3 'i3lock -c 000000' 'killall feh' --timer 2 'xset dpms force off' 'killall feh'
--- files/scripts/screenlock.sh
+++ files/scripts/screenlock.sh
@@ -4,21 +4,22 @@
 export PRIMARY_DISPLAY="$(xrandr | grep primary | cut -f 1 -d ' ')"
 
 # Run xidlehook
-xidlehook \
-	`# Don't lock when fullscreen` \
-	--not-when-fullscreen \
-	`# Dim the screen after X seconds, undim if user becomes active` \
-	--timer	180 \
-	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
-	  'feh -F ~/dots/media/aperture_laboratories.jpg' \
-	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
-	  'killall feh' \
-	`# Lock screen after another X seconds, full bright if user active` \
-	--timer 60 \
-	  'i3lock -c 000000' \
-	  'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-	`# Turn off screen after another X seconds, full bright if user active` \
-	--timer 10 \
-	  'xset dpms force off' \
-	  'killall feh' \
-	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1`
+# xidlehook \
+#	`# Don't lock when fullscreen` \
+#	--not-when-fullscreen \
+#	`# Dim the screen after X seconds, undim if user becomes active` \
+#	--timer	180 \
+#	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
+#	  'feh -F ~/dots/media/aperture_laboratories.jpg' \
+#	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
+#	  'killall feh' \
+#	`# Lock screen after another X seconds, full bright if user active` \
+#	--timer 60 \
+#	  'i3lock -c 000000' \
+#	  'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+#	`# Turn off screen after another X seconds, full bright if user active` \
+#	--timer 10 \
+#	  'xset dpms force off' \
+#	  'killall feh' \
+#	  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1`
+xidlehook --timer 180 'feh -F ~/dots/media/aperture_laboratories.jpg' 'killall feh' --timer 60 'i3lock -c 000000' 'killall feh' --timer 10 'xset dpms force off' 'killall feh'
