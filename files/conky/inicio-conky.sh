4aac46c474daa31080f2c04aab4153d686b0adf7
Carlos <carlos.manoso@udc.es>
2020-08-06 22:01:13 +0200

    Finally updated conky

--- files/conky/inicio-conky.sh
+++ files/conky/inicio-conky.sh
@@ -1,5 +1,8 @@
-# {{@@ header() @@}} #
-
 #!/bin/bash
-sleep 15 && conky -c ~/.conkyrc
-sleep 2 && conky -c ~/.conkyrc2
+
+sleep 7
+
+if ! pgrep -x "conky"; then
+	conky -c ~/.conkyrc
+	sleep 1 && conky -c ~/.conkyrc2
+fi
