5cada64f85901395ae71013404b990abb1424de3
Carlos <carloshcr2@gmail.com>
2021-08-05 13:47:23 +0200

    Updated nvim config for languagetool

--- files/nvim/lua/config/languagetool.lua
+++ files/nvim/lua/config/languagetool.lua
@@ -1,8 +1,5 @@
 -- LanguageTool config
 ----------------------
 
--- Location of the languagetool-commandline.jar file
-vim.g.languagetool_jar='$HOME/Programas/languagetool/LanguageTool-5.4/languagetool-commandline.jar'
-
 -- Config to use languagetool with Latex
 vim.g.languagetool_cmd = '$HOME/Programas/languagetool/yalafi'
