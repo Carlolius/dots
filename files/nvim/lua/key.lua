416cabfb7dec1c0dbaed99e8b312158c4131fcc0
Carlos <carloshcr2@gmail.com>
2021-12-28 02:23:29 +0100

    Move text in visual mode

--- files/nvim/lua/key.lua
+++ files/nvim/lua/key.lua
@@ -17,10 +17,9 @@ keymap('n', '<S-h>', ':tabprevious<CR>', { noremap = true })
 -- Navigate buffers
 keymap('n', '<S-j>', ':bnext<CR>', { noremap = true })
 keymap('n', '<S-k>', ':bprevious<CR>', { noremap = true })
--- Move text up and down
-keymap("v", "<A-j>", ":move .+1<CR>==", { noremap = true })
-keymap("v", "<A-k>", ":move .-2<CR>==", { noremap = true })
-keymap("v", "p", '"_dP', { noremap = true })
+-- Move text in visual mode with J/K
+keymap("v", "<A-j>", [[:m '>+1<CR>gv=gv]], { noremap = true })
+keymap("v", "<A-k>", [[:m '<-2<CR>gv=gv]], { noremap = true })
 -- Indent in visual mode
 keymap("v", "<A-l>", ">+1<CR>gv-gv", { noremap = true })
 keymap("v", "<A-h>", "<-2<CR>gv-gv", { noremap = true })
