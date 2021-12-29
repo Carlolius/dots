a8d1071dad58bc823bc3d84bb0a97422a6496b4c
Carlos <carloshcr2@gmail.com>
2021-12-30 00:18:23 +0100

    Changes on whichkey

--- files/nvim/lua/config/which-key-config.lua
+++ files/nvim/lua/config/which-key-config.lua
@@ -70,17 +70,6 @@ local setup = {
   },
 }
 
-local opts = {
-  mode = "n", -- NORMAL mode
-  -- prefix: use "<leader>f" for example for mapping everything related to finding files
-  -- the prefix is prepended to every mapping part of `mappings`
-  prefix = "",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = false, -- use `nowait` when creating keymaps
-}
-
 local mappings = {
 	["e"] = { "<cmd>NvimTreeToggle<cr>", "Toogle NvimTree" },
 	["b"] = {
@@ -215,5 +204,4 @@ local mappings = {
 }
 
 wk.setup(setup)
-wk.register(mappings, opts)
 wk.register(mappings, { prefix = "<leader>" })
