ec5ced71e60aec953e00bfa2924f68c8fd6b3acf
Carlos <carloshcr2@gmail.com>
2022-01-16 23:49:23 +0100

    Config copied somewhere

--- files/nvim/lua/config/feline-statusline.lua
+++ files/nvim/lua/config/feline-statusline.lua
@@ -272,6 +272,40 @@ local flsp = {
 	}
 }
 
+local lsp_progress = {
+   provider = function()
+      local Lsp = vim.lsp.util.get_progress_messages()[1]
+
+      if Lsp then
+         local msg = Lsp.message or ""
+         local percentage = Lsp.percentage or 0
+         local title = Lsp.title or ""
+         local spinners = {
+            "",
+            "",
+            "",
+         }
+
+         local success_icon = {
+            "",
+            "",
+            "",
+         }
+
+         local ms = vim.loop.hrtime() / 1000000
+         local frame = math.floor(ms / 120) % #spinners
+
+         if percentage >= 70 then
+            return string.format(" %%<%s %s %s (%s%%%%) ", success_icon[frame + 1], title, msg, percentage)
+         end
+
+         return string.format(" %%<%s %s %s (%s%%%%) ", spinners[frame + 1], title, msg, percentage)
+      end
+
+      return ""
+   end,
+   hl = { fg = colors.green },
+}
 
 local git = {
 	branch = {
@@ -313,7 +347,8 @@ components.active[1] = {
 	file.info,
 	file.type,
 	file.size,
-	flsp.name
+	flsp.name,
+	lsp_progress,
 }
 
 components.inactive[1] = {
--- files/nvim/lua/config/treesitter-config.lua
+++ files/nvim/lua/config/treesitter-config.lua
@@ -34,4 +34,15 @@ require'nvim-treesitter.configs'.setup {
 	indent = {
 		enable = true
 	},
+	autotag = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	refactor = {
+		highlight_definitions = { enable = true },
+		highlight_current_scope = { enable = false },
+	},
 }
