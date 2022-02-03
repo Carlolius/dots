4be37136433084797e1eb370435a4c460a67ac1a
Carlos <carloshcr2@gmail.com>
2022-02-04 00:18:38 +0100

    Not overmapping

--- files/nvim/lua/key.lua
+++ files/nvim/lua/key.lua
@@ -12,14 +12,14 @@ keymap("n", "<Leader><space>", ":nohlsearch<cr>", { noremap = true })
 keymap("i", "jk", "<ESC>", { noremap = true })
 keymap("i", "kj", "<ESC>", { noremap = true })
 -- Navigate tabs
-keymap("n", "<S-l>", ":tabnext<CR>", { noremap = true })
-keymap("n", "<S-h>", ":tabprevious<CR>", { noremap = true })
+keymap("n", "<A-l>", ":tabnext<CR>", { noremap = true })
+keymap("n", "<A-h>", ":tabprevious<CR>", { noremap = true })
 -- Navigate buffers
-keymap("n", "<S-j>", ":bnext<CR>", { noremap = true })
-keymap("n", "<S-k>", ":bprevious<CR>", { noremap = true })
+keymap("n", "<A-j>", ":bnext<CR>", { noremap = true })
+keymap("n", "<A-k>", ":bprevious<CR>", { noremap = true })
 -- Move text in visual mode with J/K
-keymap("v", "<A-j>", [[:m '>+1<CR>gv=gv]], { noremap = true })
-keymap("v", "<A-k>", [[:m '<-2<CR>gv=gv]], { noremap = true })
+keymap("v", "<A-S-j>", [[:m '>+1<CR>gv=gv]], { noremap = true })
+keymap("v", "<A-S-k>", [[:m '<-2<CR>gv=gv]], { noremap = true })
 -- Indent in visual mode
-keymap("v", "<A-l>", ">+1<CR>gv-gv", { noremap = true })
-keymap("v", "<A-h>", "<-2<CR>gv-gv", { noremap = true })
+keymap("v", "<A-S-l>", ">+1<CR>gv-gv", { noremap = true })
+keymap("v", "<A-S-h>", "<-2<CR>gv-gv", { noremap = true })
