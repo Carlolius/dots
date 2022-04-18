16b9a5f7b6a761a0f655e5a24cad34cc3bf1d315
Carlos <carloshcr2@gmail.com>
2022-04-18 21:09:30 +0200

    Grammarguard language change

--- files/nvim/lua/config/lsp/settings/grammarguard.lua
+++ files/nvim/lua/config/lsp/settings/grammarguard.lua
@@ -6,9 +6,11 @@ return({
 		ltex = {
 			enabled = { "latex", "tex", "bib", "markdown" },
 			-- English: en
+			-- language = "en",
 			-- Spanish: es
+			language = "es",
 			-- Galician: gl-ES
-			language = "gl-ES",
+			-- language = "gl-ES",
 			diagnosticSeverity = "information",
 			setenceCacheSize = 2000,
 			additionalRules = {
