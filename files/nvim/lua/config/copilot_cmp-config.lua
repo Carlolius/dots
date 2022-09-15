bf52791c6bdc3ab1bdfad87b05cece6ff24f60d0
Carlos <carloshcr2@gmail.com>
2022-09-15 14:00:24 +0200

    Fixed copilot format

--- files/nvim/lua/config/copilot_cmp-config.lua
+++ files/nvim/lua/config/copilot_cmp-config.lua
@@ -3,7 +3,7 @@ require("copilot_cmp").setup {
   force_autofmt = false,
   formatters = {
     label = require("copilot_cmp.format").format_label_text,
-    insert_text = require("copilot_cmp.format").format_label_text,
+    insert_text = require("copilot_cmp.format").remove_existing,
     preview = require("copilot_cmp.format").deindent,
   },
 }
