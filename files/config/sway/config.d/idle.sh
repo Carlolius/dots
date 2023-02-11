501b61cdcedfd10a9da39e29a2e87b12ec568526
carlos <carloshcr2@gmail.com>
2023-02-12 00:27:55 +0100

    litte changes on sway and waybar

--- files/config/sway/config.d/idle.sh
+++ files/config/sway/config.d/idle.sh
@@ -4,5 +4,5 @@ exec swayidle -w \
 	resume 'swaymsg "output * dpms on"' \
 	timeout 7 'pgrep swaylock && swaymsg "output * dpms off"' \
  	resume 'pgrep swaylock && swaymsg "output * dpms on"' \
-	before-sleep 'swaylock -f -c 111111' \
+	before-sleep 'swaylock -c 111111' \
 	after-resume 'swaymsg "output * dpms on"' &
