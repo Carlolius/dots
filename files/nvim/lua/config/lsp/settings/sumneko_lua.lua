482a0d141eb74f79d347d8949b84656e0d6953bf
Carlos <carloshcr2@gmail.com>
2023-05-19 15:29:49 +0200

    Updated LSP servers
    A lot of different stuff to make the lsp work. Still not working the ruby_lsp

---
+++ files/nvim/lua/config/lsp/settings/sumneko_lua.lua
@@ -0,0 +1,19 @@
+-- Sumneko lua config
+---------------------
+
+return{
+	settings = {
+
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+}
--- files/nvim/lua/config/mason-config.lua
+++ files/nvim/lua/config/mason-config.lua
@@ -1,21 +1,37 @@
-require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
-require('mason-lspconfig').setup()
-
 local lspconfig = require('lspconfig')
 local lsp_defaults = lspconfig.util.default_config
 
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
+})
 
-lspconfig.lua_ls.setup({})
+local handlers = {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- default handler (optional)
+		local opts = {
+			on_attach = require("config.lsp.handlers").on_attach,
+			capabilities = require("config.lsp.handlers").capabilities,
+		}
+		local has_custom_opts, server_custom_opts = pcall(require, "config.lsp.settings." .. server_name)
+		if has_custom_opts then
+			opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+		end
+		lspconfig[server_name].setup(opts)
+	end
+}
+
+require('mason-lspconfig').setup_handlers(handlers)
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+	'force',
+	lsp_defaults.capabilities,
+	require('cmp_nvim_lsp').default_capabilities()
+)
