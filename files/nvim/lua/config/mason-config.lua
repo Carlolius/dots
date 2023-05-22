3b7590305b6c4138f491e9f8389f8aeeeaac706e
carlos <carloshcr2@gmail.com>
2023-05-22 10:01:08 +0200

    Some format and other changes

--- files/nvim/lua/config/mason-config.lua
+++ files/nvim/lua/config/mason-config.lua
@@ -1,7 +1,34 @@
-local lspconfig = require('lspconfig')
+------------------
+-- Mason config --
+------------------
+
+local mason_status_ok, mason = pcall(require, "mason")
+if not mason_status_ok then
+	vim.notify("Couldn't load Mason" .. mason, "error")
+	return
+end
+
+local mason_lspconfig_status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status_ok then
+	vim.notify("Couldn't load Mason-LSP-Config" .. mason_lspconfig, "error")
+	return
+end
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	vim.notify("Couldn't load LSP-Config" .. lspconfig, "error")
+	return
+end
+
+local cmp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_status_ok then
+	vim.notify("Couldn't load CMP" .. cmp_nvim_lsp, "error")
+	return
+end
+
 local lsp_defaults = lspconfig.util.default_config
 
-require("mason").setup({
+mason.setup({
 	ui = {
 		icons = {
 			package_installed = "✓",
@@ -28,10 +55,10 @@ local handlers = {
 	end
 }
 
-require('mason-lspconfig').setup_handlers(handlers)
+mason_lspconfig.setup_handlers(handlers)
 
 lsp_defaults.capabilities = vim.tbl_deep_extend(
 	'force',
 	lsp_defaults.capabilities,
-	require('cmp_nvim_lsp').default_capabilities()
+	cmp_nvim_lsp.default_capabilities()
 )
