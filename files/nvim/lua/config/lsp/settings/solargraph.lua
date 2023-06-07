fd919b551c448c2b923cf113d704abba84849521
Carlos <carloshcr2@gmail.com>
2023-06-07 17:32:03 +0200

    Changes in lsp

---
+++ files/nvim/lua/config/lsp/settings/solargraph.lua
@@ -0,0 +1,11 @@
+-------------
+-- Solargraph
+-------------
+
+return {
+  settings = {
+    Solargraph = {
+      root_dir = require("lspconfig").util.root_pattern("Gemfile", ".git")(fname) or vim.fn.getcwd()
+    },
+  },
+}
