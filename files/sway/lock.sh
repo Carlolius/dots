2ded8f78815c532a789c2fc5368469a52a657cb8
Carlol <carloshcr2@gmail.com>
2025-04-25 09:28:18 +0200

    Sway changes

--- files/sway/lock.sh
+++ files/sway/lock.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-swaylock -f --screenshots --clock --indicator-idle-visible \
-	--grace 7 \
-	--hide-keyboard-layout \
-	--indicator-radius 100 \
-	--indicator-thickness 7 \
-	--ring-color 455a64 \
-	--key-hl-color be5046 \
-	--text-color ffc107 \
-	--line-color 00000000 \
-	--inside-color 00000088 \
-	--separator-color 00000000 \
-	--fade-in 3 \
-	--effect-scale 0.5 --effect-blur 10x4 --effect-scale 5 \
-	--effect-vignette 0.5:0.5 \
-	"$@"
+swaylock \
+  --color 000000 \
+  -e \
+  --indicator-radius 150 \
+  --indicator-thickness 20 \
+  --inside-color 000000 \
+  --inside-clear-color 388697 \
+  --inside-ver-color 00FF00 \
+  --inside-wrong-color ff0000 \
+  --line-color FFFFFF \
+  --ring-color 0000FF \
+  --key-hl-color 00FF00 \
+  --line-color 00000000 \
+  --separator-color 373b41 \
